1c6851989e4789d55911bfd6ceb0aa27f54e33a7
lvca <lvca@users.noreply.github.com>
2021-08-26 00:23:04 -0400

    Added asciiart at startup

--- engine/src/main/java/com/arcadedb/Constants.java
+++ engine/src/main/java/com/arcadedb/Constants.java
@@ -29,126 +29,123 @@ import java.util.Properties;
 import java.util.logging.Level;
 
 public class Constants {
-    public static final String PRODUCT = "ArcadeDB";
-    public static final String URL = "https://arcadedata.com";
-    public static final String COPYRIGHT = "Copyrights (c) 2021 Arcade Data";
+  public static final String PRODUCT   = "ArcadeDB";
+  public static final String URL       = "https://arcadedb.com";
+  public static final String COPYRIGHT = "Copyrights (c) 2021 Arcade Data";
 
-    private static final Properties properties = new Properties();
+  private static final Properties properties = new Properties();
 
-    static {
-        final InputStream inputStream = Constants.class.getResourceAsStream("/com/arcadedb/arcadedb.properties");
+  static {
+    final InputStream inputStream = Constants.class.getResourceAsStream("/com/arcadedb/arcadedb.properties");
+    try {
+      properties.load(inputStream);
+    } catch (IOException e) {
+      LogManager.instance().log(Constants.class, Level.SEVERE, "Failed to load ArcadeDB properties", e);
+    } finally {
+      if (inputStream != null) {
         try {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            LogManager.instance().log(Constants.class, Level.SEVERE, "Failed to load ArcadeDB properties", e);
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException ignore) {
-                    // Ignore
-                }
-            }
+          inputStream.close();
+        } catch (IOException ignore) {
+          // Ignore
         }
-
+      }
     }
 
-    /**
-     * @return Major part of Arcadedb version
-     */
-    public static int getVersionMajor() {
-        final String[] versions = properties.getProperty("version").split("\\.");
-        if (versions.length == 0) {
-            LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve version information for this build", null);
-            return -1;
-        }
+  }
 
-        try {
-            return Integer.parseInt(versions[0]);
-        } catch (NumberFormatException nfe) {
-            LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve major version information for this build", nfe);
-            return -1;
-        }
+  /**
+   * @return Major part of Arcadedb version
+   */
+  public static int getVersionMajor() {
+    final String[] versions = properties.getProperty("version").split("\\.");
+    if (versions.length == 0) {
+      LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve version information for this build", null);
+      return -1;
     }
 
-    /**
-     * @return Minor part of Arcadedb version
-     */
-    public static int getVersionMinor() {
-        final String[] versions = properties.getProperty("version").split("\\.");
-        if (versions.length < 2) {
-            LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve minor version information for this build", null);
-            return -1;
-        }
+    try {
+      return Integer.parseInt(versions[0]);
+    } catch (NumberFormatException nfe) {
+      LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve major version information for this build", nfe);
+      return -1;
+    }
+  }
 
-        try {
-            return Integer.parseInt(versions[1]);
-        } catch (NumberFormatException nfe) {
-            LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve minor version information for this build", nfe);
-            return -1;
-        }
+  /**
+   * @return Minor part of Arcadedb version
+   */
+  public static int getVersionMinor() {
+    final String[] versions = properties.getProperty("version").split("\\.");
+    if (versions.length < 2) {
+      LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve minor version information for this build", null);
+      return -1;
     }
 
-    /**
-     * @return Hotfix part of Arcadedb version
-     */
-    @SuppressWarnings("unused")
-    public static int getVersionHotfix() {
-        final String[] versions = properties.getProperty("version").split("\\.");
-        if (versions.length < 3) {
-            return 0;
-        }
+    try {
+      return Integer.parseInt(versions[1]);
+    } catch (NumberFormatException nfe) {
+      LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve minor version information for this build", nfe);
+      return -1;
+    }
+  }
 
-        try {
-            String hotfix = versions[2];
-            int snapshotIndex = hotfix.indexOf("-SNAPSHOT");
-
-            if (snapshotIndex != -1) {
-                hotfix = hotfix.substring(0, snapshotIndex);
-            }
-
-            return Integer.parseInt(hotfix);
-        } catch (NumberFormatException nfe) {
-            LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve hotfix version information for this build", nfe);
-            return -1;
-        }
+  /**
+   * @return Hotfix part of Arcadedb version
+   */
+  @SuppressWarnings("unused")
+  public static int getVersionHotfix() {
+    final String[] versions = properties.getProperty("version").split("\\.");
+    if (versions.length < 3) {
+      return 0;
     }
 
-    /**
-     * @return Returns only current version without build number and etc.
-     */
-    public static String getRawVersion() {
-        return properties.getProperty("version");
-    }
+    try {
+      String hotfix = versions[2];
+      int snapshotIndex = hotfix.indexOf("-SNAPSHOT");
 
-    /**
-     * Returns the complete text of the current Arcadedb version.
-     */
-    public static String getVersion() {
-        return getRawVersion()
-                + " (build " + getBuildNumber()
-                + " timestamp " + getTimestamp()
-                + ", branch " + properties.getProperty("branch") + ")";
-    }
+      if (snapshotIndex != -1) {
+        hotfix = hotfix.substring(0, snapshotIndex);
+      }
 
-    /**
-     * Returns true if current Arcadedb version is a snapshot.
-     */
-    public static boolean isSnapshot() {
-        return properties.getProperty("version").endsWith("SNAPSHOT");
+      return Integer.parseInt(hotfix);
+    } catch (NumberFormatException nfe) {
+      LogManager.instance().log(Constants.class, Level.SEVERE, "Can not retrieve hotfix version information for this build", nfe);
+      return -1;
     }
+  }
 
-    /**
-     * @return the build number if any.
-     */
-    public static String getBuildNumber() {
-        return properties.getProperty("buildNumber");
-    }
+  /**
+   * @return Returns only current version without build number and etc.
+   */
+  public static String getRawVersion() {
+    return properties.getProperty("version");
+  }
 
-    /**
-     * @return the build number if any.
-     */
-    public static String getTimestamp() {
-        return properties.getProperty("timestamp");
-    }
+  /**
+   * Returns the complete text of the current Arcadedb version.
+   */
+  public static String getVersion() {
+    return getRawVersion() + " (build " + getBuildNumber() + " timestamp " + getTimestamp() + ", branch " + properties.getProperty("branch") + ")";
+  }
+
+  /**
+   * Returns true if current Arcadedb version is a snapshot.
+   */
+  public static boolean isSnapshot() {
+    return properties.getProperty("version").endsWith("SNAPSHOT");
+  }
+
+  /**
+   * @return the build number if any.
+   */
+  public static String getBuildNumber() {
+    return properties.getProperty("buildNumber");
+  }
+
+  /**
+   * @return the build number if any.
+   */
+  public static String getTimestamp() {
+    return properties.getProperty("timestamp");
+  }
 }
