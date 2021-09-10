03b147016470230a824c34045286a2f87e2fcaac
lvca <lvca@users.noreply.github.com>
2021-09-10 14:45:01 -0400

    Fixed create index sql test case

--- engine/src/test/java/com/arcadedb/query/sql/parser/CreateIndexStatementTest.java
+++ engine/src/test/java/com/arcadedb/query/sql/parser/CreateIndexStatementTest.java
@@ -20,9 +20,8 @@ public class CreateIndexStatementTest extends ParserTestAbstract {
     checkRightSyntax("CREATE INDEX Foo.bar_baz on Foo (bar, baz) UNIQUE");
     checkRightSyntax("CREATE INDEX Foo.bar_baz on Foo (bar by key, baz by value) UNIQUE");
 
-    checkRightSyntax("CREATE INDEX test unique string,string");
-    checkRightSyntax("create index OUser.name UNIQUE ENGINE LSM STRING");
-    checkRightSyntax("create index OUser.name UNIQUE engine LSM STRING");
+    checkRightSyntax("create index OUser.name UNIQUE ENGINE LSM");
+    checkRightSyntax("create index OUser.name UNIQUE engine LSM");
 
     checkRightSyntax("CREATE INDEX Foo.bar IF NOT EXISTS on Foo (bar) UNIQUE");
     checkWrongSyntax("CREATE INDEX Foo.bar IF EXISTS on Foo (bar) UNIQUE");
