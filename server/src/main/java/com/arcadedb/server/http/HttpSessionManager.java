c040371c6a48c165953edfecb7926a9c56888752
lvca <lvca@users.noreply.github.com>
2021-11-02 12:56:51 -0400

    Minor refactoring

--- server/src/main/java/com/arcadedb/server/http/HttpSessionManager.java
+++ server/src/main/java/com/arcadedb/server/http/HttpSessionManager.java
@@ -87,4 +87,8 @@ public class HttpSessionManager extends RWLockContext {
   public HttpSession removeSession(final String iSessionId) {
     return executeInWriteLock(() -> sessions.remove(iSessionId));
   }
+
+  public int getActiveSessions() {
+    return sessions.size();
+  }
 }
