efe8000fc9cef25d65bf8f7871499778173db991
lvca <lvca@users.noreply.github.com>
2021-11-04 15:08:43 -0400

    Upgraded to Gremlin 3.5.1

--- gremlin/src/test/java/org/apache/tinkerpop/gremlin/arcadedb/ArcadeGraphProvider.java
+++ gremlin/src/test/java/org/apache/tinkerpop/gremlin/arcadedb/ArcadeGraphProvider.java
@@ -16,7 +16,7 @@
 package org.apache.tinkerpop.gremlin.arcadedb;
 
 import com.arcadedb.database.RID;
-import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration2.Configuration;
 import org.apache.tinkerpop.gremlin.AbstractGraphProvider;
 import org.apache.tinkerpop.gremlin.LoadGraphWith;
 import org.apache.tinkerpop.gremlin.arcadedb.structure.ArcadeEdge;
