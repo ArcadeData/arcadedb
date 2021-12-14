97b1731166788b3608a3f74940181a35bae37f73
lvca <lvca@users.noreply.github.com>
2021-12-14 09:28:52 -0500

    Fixed Gremlin Server database path
    Fixed issue #213

---
+++ server/src/test/java/com/arcadedb/server/gremlin/ConnectRemoteGremlinServer.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2021-present Arcade Data Ltd (info@arcadedata.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arcadedb.server.gremlin;
+
+import org.apache.tinkerpop.gremlin.arcadedb.structure.io.ArcadeIoRegistry;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.driver.ser.GraphBinaryMessageSerializerV1;
+import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.WithOptions;
+import org.apache.tinkerpop.gremlin.structure.io.binary.TypeSerializerRegistry;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static com.arcadedb.server.BaseGraphServerTest.DEFAULT_PASSWORD_FOR_TESTS;
+
+/**
+ * Manual test against a Gremlin Server.
+ */
+public class ConnectRemoteGremlinServer {
+
+  @Disabled
+  @Test
+  public void getAllVertices() {
+    final GraphTraversalSource g = traversal();
+
+    g.addV().property("myProp", "some value").next(); // creates the vertex
+    List<Map<Object, Object>> vertices = g.V().valueMap().with(WithOptions.tokens).toList(); // verifies that the vertex created with properties is returned
+
+    Assertions.assertFalse(vertices.isEmpty());
+  }
+
+  private Cluster createCluster() {
+    final GraphBinaryMessageSerializerV1 serializer = new GraphBinaryMessageSerializerV1(
+        new TypeSerializerRegistry.Builder().addRegistry(new ArcadeIoRegistry()));
+
+    return Cluster.build().enableSsl(false).addContactPoint("localhost").port(8182).credentials("root", DEFAULT_PASSWORD_FOR_TESTS).serializer(serializer)
+        .create();
+  }
+
+  private GraphTraversalSource traversal() {
+    return AnonymousTraversalSource.traversal().withRemote(DriverRemoteConnection.using(createCluster(), "graph"));
+  }
+}
