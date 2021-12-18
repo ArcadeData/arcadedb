5ebcbdb934290bebf23f4cd6f9d77780db7181f9
arcade-player <arcade@arcadeanalytics.com>
2021-12-18 18:45:02 +0100

    jacoco files find command and export

--- server/src/main/java/com/arcadedb/server/TestCallback.java
+++ server/src/main/java/com/arcadedb/server/TestCallback.java
@@ -16,7 +16,18 @@
 package com.arcadedb.server;
 
 public interface TestCallback {
-  enum TYPE {SERVER_STARTING, SERVER_UP, SERVER_SHUTTING_DOWN, SERVER_DOWN, REPLICA_MSG_RECEIVED, REPLICA_ONLINE, REPLICA_OFFLINE, REPLICA_HOT_RESYNC, REPLICA_FULL_RESYNC, NETWORK_CONNECTION}
+    enum TYPE {
+        SERVER_STARTING,
+        SERVER_UP,
+        SERVER_SHUTTING_DOWN,
+        SERVER_DOWN,
+        REPLICA_MSG_RECEIVED,
+        REPLICA_ONLINE,
+        REPLICA_OFFLINE,
+        REPLICA_HOT_RESYNC,
+        REPLICA_FULL_RESYNC,
+        NETWORK_CONNECTION
+    }
 
-  void onEvent(TYPE type, Object object, ArcadeDBServer server) throws Exception;
+    void onEvent(TYPE type, Object object, ArcadeDBServer server) throws Exception;
 }
