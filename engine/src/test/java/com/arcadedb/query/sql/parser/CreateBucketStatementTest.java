8e3bb883d9ce2389693d94cbd35d691262effd53
lvca <lvca@users.noreply.github.com>
2022-01-07 02:33:36 -0500

    SQL: removed unused keyword inherited from OrientDB

--- engine/src/test/java/com/arcadedb/query/sql/parser/CreateBucketStatementTest.java
+++ engine/src/test/java/com/arcadedb/query/sql/parser/CreateBucketStatementTest.java
@@ -25,18 +25,10 @@ public class CreateBucketStatementTest extends ParserTestAbstract {
     checkRightSyntax("CREATE BUCKET Foo ID 14");
     checkRightSyntax("create bucket Foo");
     checkRightSyntax("create bucket Foo id 14");
-    checkRightSyntax("CREATE BLOB BUCKET Foo");
-    checkRightSyntax("create blob bucket Foo id 14");
-
-    checkRightSyntax("create blob bucket Foo IF NOT EXISTS");
-    checkRightSyntax("create blob bucket Foo IF NOT EXISTS id 14");
 
     checkWrongSyntax("CREATE Bucket");
     checkWrongSyntax("CREATE Bucket foo bar");
     checkWrongSyntax("CREATE Bucket foo.bar");
     checkWrongSyntax("CREATE Bucket foo id bar");
-
-    checkWrongSyntax("create blob bucket Foo IF EXISTS");
-    checkWrongSyntax("create blob bucket Foo IF EXISTS id 14");
   }
 }
