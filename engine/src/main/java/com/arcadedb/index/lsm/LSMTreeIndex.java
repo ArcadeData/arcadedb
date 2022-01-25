351583fc5ccc8f54f0c9217f6d3eee02cd41caa1
lvca <lvca@users.noreply.github.com>
2022-01-25 16:36:03 -0500

    Added copyright and license in most of the files

--- engine/src/main/java/com/arcadedb/index/lsm/LSMTreeIndex.java
+++ engine/src/main/java/com/arcadedb/index/lsm/LSMTreeIndex.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: 2021-present Arcade Data Ltd (info@arcadedata.com)
+ * SPDX-License-Identifier: Apache-2.0
  */
 package com.arcadedb.index.lsm;
 
