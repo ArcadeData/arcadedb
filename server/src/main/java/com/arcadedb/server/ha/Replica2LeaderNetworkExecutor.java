6c4b431a341ede11d085d70ffc8c5b0c175e2723
lvca <lvca@users.noreply.github.com>
2023-01-14 00:25:44 -0500

    chore:added more logs on server db installation

--- server/src/main/java/com/arcadedb/server/ha/Replica2LeaderNetworkExecutor.java
+++ server/src/main/java/com/arcadedb/server/ha/Replica2LeaderNetworkExecutor.java
@@ -428,18 +428,31 @@ public class Replica2LeaderNetworkExecutor extends Thread {
       schemaFile.write(dbStructure.getSchemaJson());
     }
 
+    long databaseSize = 0L;
     // WRITE ALL THE FILES
-    for (final Map.Entry<Integer, String> f : dbStructure.getFileNames().entrySet()) {
-      installFile(buffer, db, f.getKey(), f.getValue(), 0, -1);
+    final List<Map.Entry<Integer, String>> list = new ArrayList<>(dbStructure.getFileNames().entrySet());
+    for (int i = 0; i < list.size(); i++) {
+      final Map.Entry<Integer, String> f = list.get(i);
+      try {
+        databaseSize += installFile(buffer, db, f.getKey(), f.getValue(), 0, -1);
+      } catch (Exception e) {
+        LogManager.instance()
+            .log(this, Level.SEVERE, "Error on installing file '%s' (%s %d/%d files)", e, f.getKey(), FileUtils.getSizeAsString(databaseSize), i, list.size());
+        database.getEmbedded().drop();
+        throw new ReplicationException("Error on installing database '" + db + "'", e);
+      }
     }
 
     // RELOAD THE SCHEMA
     database.getSchema().getEmbedded().close();
     DatabaseContext.INSTANCE.init(database);
     database.getSchema().getEmbedded().load(PaginatedFile.MODE.READ_WRITE, true);
+
+    LogManager.instance()
+        .log(this, Level.INFO, "Database '%s' installed from the cluster (%s - %d files)", null, db, FileUtils.getSizeAsString(databaseSize), list.size());
   }
 
-  private void installFile(final Binary buffer, final String db, final int fileId, final String fileName, final int pageFromInclusive,
+  private long installFile(final Binary buffer, final String db, final int fileId, final String fileName, final int pageFromInclusive,
       final int pageToInclusive) throws IOException {
 
     int from = pageFromInclusive;
@@ -447,10 +460,12 @@ public class Replica2LeaderNetworkExecutor extends Thread {
     LogManager.instance().log(this, Level.FINE, "Installing file '%s'...", fileName);
 
     int pagesWritten = 0;
-    final long fileSize = 0;
+    long fileSize = 0;
     while (true) {
       sendCommandToLeader(buffer, new FileContentRequest(db, fileId, from, pageToInclusive), -1);
       final FileContentResponse fileChunk = (FileContentResponse) receiveCommandFromLeaderDuringJoin(buffer);
+
+      fileSize += fileChunk.getPagesContent().size();
 
       fileChunk.execute(server, null, -1);
 
@@ -466,6 +481,8 @@ public class Replica2LeaderNetworkExecutor extends Thread {
     }
 
     LogManager.instance().log(this, Level.FINE, "File '%s' installed (pagesWritten=%d size=%s)", fileName, pagesWritten, FileUtils.getSizeAsString(fileSize));
+
+    return fileSize;
   }
 
   private HACommand receiveCommandFromLeaderDuringJoin(final Binary buffer) throws IOException {
