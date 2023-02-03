439be1331cdf905ee4455a95da8ace940878638f
Gregor Frey <g.k.frey@gmail.com>
2023-02-03 20:14:26 +0100

    start test for exists() in RemoteDatabase

--- network/src/test/java/com/arcadedb/remote/RemoteDatabaseTest.java
+++ network/src/test/java/com/arcadedb/remote/RemoteDatabaseTest.java
@@ -32,4 +32,23 @@ class RemoteDatabaseTest {
         JSONObject payload = new JSONObject("{\"command\":\"create database null\",\"serializer\":\"record\"}");
         verify(database).setRequestPayload(connection, payload);
     }
+
+    @Test
+    void testExists() throws Exception {
+        HttpURLConnection connection = mock(HttpURLConnection.class);
+        doNothing().when(connection).connect();
+        when(connection.getResponseCode()).thenReturn(200);
+        when(connection.getInputStream()).thenReturn(new ByteArrayInputStream("{}".getBytes()));
+
+        RemoteDatabase database = mock(RemoteDatabase.class, CALLS_REAL_METHODS);
+        database.setApiVersion(1);
+        doNothing().when(database).setRequestPayload(any(), any());
+        doReturn(connection).when(database).createConnection(any(), any());
+        database.exists();
+        verify(database).httpCommand("POST", null, "server", null,
+                "create database null", null, true, true, null);
+        verify(database).createConnection("POST", "http://null:0/api/v1/server");
+        JSONObject payload = new JSONObject("{\"command\":\"create database null\",\"serializer\":\"record\"}");
+        verify(database).setRequestPayload(connection, payload);
+    }
 }
