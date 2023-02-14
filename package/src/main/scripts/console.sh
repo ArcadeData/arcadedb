a717ca1e83649f98e6c011fd81fedaf313a8f091
lvca <lvca@users.noreply.github.com>
2023-02-14 12:46:23 -0500

    chore: removed warning at console startup

--- package/src/main/scripts/console.sh
+++ package/src/main/scripts/console.sh
@@ -48,7 +48,7 @@ fi
 
 
 if [ -z "$JAVA_OPTS_SCRIPT" ] ; then
-    JAVA_OPTS_SCRIPT="-XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Dpolyglot.engine.WarnInterpreterOnly=false"
+    JAVA_OPTS_SCRIPT="-XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Dpolyglot.engine.WarnInterpreterOnly=false --illegal-access=deny"
 fi
 
 if [ $# -gt 0 ] ; then
