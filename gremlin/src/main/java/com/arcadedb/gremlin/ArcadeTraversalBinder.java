0d4cd893541dcd981f78efbfde5b555b8b46c7ec
lvca <lvca@users.noreply.github.com>
2023-09-14 18:07:47 -0400

    Resolved issue with Java11 compilation

--- gremlin/src/main/java/com/arcadedb/gremlin/ArcadeTraversalBinder.java
+++ gremlin/src/main/java/com/arcadedb/gremlin/ArcadeTraversalBinder.java
@@ -5,30 +5,30 @@ import org.apache.tinkerpop.gremlin.jsr223.GremlinScriptEngine;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * Binds a tinkerpop `TraversalSource` to a named script binding. See the following tinkerpop
  * documentation for more information:
- *
+ * <p>
  * https://tinkerpop.apache.org/docs/current/reference/#gremlin-java-dsl
  * https://tinkerpop.apache.org/docs/current/reference/#traversalstrategy
  */
 public final class ArcadeTraversalBinder {
 
   private final Map<String, TraversalSupplier> traversalBindings;
-  private final Graph graph;
+  private final Graph                          graph;
 
   /**
    * Thrown in `ArcadeTraversalBinder` constructor if the `GREMLIN_TRAVERSAL_BINDINGS` configuration
    * contains invalid map entries.
-   *
+   * <p>
    * The `GlobalConfiguration.GREMLIN_TRAVERSAL_BINDINGS` is typed a a raw `Map` type in order to a
    * avoid circular dependencies on the arcadedb gremlin packages. Instead of declaring types,
    * validates the entries of the map at runtime as early as possible.
    */
   static final class IllegalTraversalBindingsEntry extends IllegalArgumentException {
-    private IllegalTraversalBindingsEntry(String message) {
+    private IllegalTraversalBindingsEntry(final String message) {
       super(message);
     }
   }
@@ -45,7 +45,7 @@ public final class ArcadeTraversalBinder {
      * class. More custom requirements to configure a `TraversalSource` with extensive additions
      * should instead implement the lambda `TraversalSupplier` interface.
      */
-    public static <T extends TraversalSource> TraversalSupplier of(Class<T> klass) {
+    static <T extends TraversalSource> TraversalSupplier of(Class<T> klass) {
       return graph -> graph.traversal(klass);
     }
 
@@ -55,31 +55,29 @@ public final class ArcadeTraversalBinder {
   /**
    * @throws IllegalTraversalBindingsEntry
    */
-  ArcadeTraversalBinder(ArcadeGraph arcadeGraph) {
+  ArcadeTraversalBinder(final ArcadeGraph arcadeGraph) {
     graph = arcadeGraph;
-    traversalBindings = arcadeGraph.getDatabase().getConfiguration()
-        .getValue(GlobalConfiguration.GREMLIN_TRAVERSAL_BINDINGS);
+    traversalBindings = arcadeGraph.getDatabase().getConfiguration().getValue(GlobalConfiguration.GREMLIN_TRAVERSAL_BINDINGS);
     validateTraversalBindings(traversalBindings);
   }
 
-  void bind(GremlinScriptEngine engine) {
+  void bind(final GremlinScriptEngine engine) {
     traversalBindings.entrySet().forEach(e -> {
-          var bindingName = e.getKey();
-          var supplier = e.getValue();
-          engine.put(bindingName, supplier.get(graph));
-        });
+      final var bindingName = e.getKey();
+      final var supplier = e.getValue();
+      engine.put(bindingName, supplier.get(graph));
+    });
   }
 
-  private static void validateTraversalBindings(Map<String, TraversalSupplier> bindings) {
+  private static void validateTraversalBindings(final Map<String, TraversalSupplier> bindings) {
     bindings.entrySet().forEach(entry -> {
-          try {
-            String.class.cast(entry.getKey());
-            TraversalSupplier.class.cast(entry.getValue());
-          } catch (ClassCastException e) {
-            throw new IllegalTraversalBindingsEntry(
-                "illegal Map<String, TraversalSupplier> entry in GREMLIN_TRAVERSAL_BINDINGS "
-                + "configuration: %s".formatted(entry));
-          }
-        });
+      try {
+        String.class.cast(entry.getKey());
+        TraversalSupplier.class.cast(entry.getValue());
+      } catch (ClassCastException e) {
+        throw new IllegalTraversalBindingsEntry(
+            String.format("illegal Map<String, TraversalSupplier> entry in GREMLIN_TRAVERSAL_BINDINGS configuration: %s", entry));
+      }
+    });
   }
 }
