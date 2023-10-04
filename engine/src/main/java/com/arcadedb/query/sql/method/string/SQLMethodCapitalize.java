02f89f797334f55468dc74f78eeb9461cb63a3cc
Christian Himpe <christian.himpe@wwu.de>
2023-10-04 20:21:36 +0200

    Add SQL method capitalize

---
+++ engine/src/main/java/com/arcadedb/query/sql/method/string/SQLMethodCapitalize.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2021-present Arcade Data Ltd (info@arcadedata.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: 2021-present Arcade Data Ltd (info@arcadedata.com)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.arcadedb.query.sql.method.string;
+
+import com.arcadedb.database.Identifiable;
+import com.arcadedb.query.sql.executor.CommandContext;
+import com.arcadedb.query.sql.method.AbstractSQLMethod;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+/**
+ * @author Christian Himpe (gramian)
+ * @author Luca Garulli (l.garulli--(at)--gmail.com)
+ */
+public class SQLMethodCapitalize extends AbstractSQLMethod {
+
+  public static final String NAME = "capitalize";
+
+  public SQLMethodCapitalize() {
+    super(NAME);
+  }
+
+  @Override
+  public Object execute(final Object iThis, final Identifiable iCurrentRecord, final CommandContext iContext, final Object ioResult, final Object[] iParams) {
+    return ioResult != null ? Pattern.compile("\\b(.)(.*?)\\b")
+                                     .matcher(ioResult.toString())
+                                     .replaceAll(match -> match.group(1).toUpperCase(Locale.ENGLISH) +
+                                                          match.group(2).toLowerCase())
+                            : null;
+  }
+}
