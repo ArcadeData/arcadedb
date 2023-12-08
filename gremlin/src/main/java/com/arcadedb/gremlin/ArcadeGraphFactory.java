89861fd1a1ad4476b5bc8292114fd350dbf367a6
lvca <lvca@users.noreply.github.com>
2023-12-08 17:09:48 -0500

    feat: new ArcadeGraphFactory to pool ArcadeGraph instances
    Fixed issue #1370

---
+++ gremlin/src/main/java/com/arcadedb/gremlin/ArcadeGraphFactory.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2023 Arcade Data Ltd
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.arcadedb.gremlin;
+
+import com.arcadedb.database.BasicDatabase;
+import com.arcadedb.remote.RemoteDatabase;
+import org.apache.commons.configuration2.Configuration;
+
+import java.io.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+
+/**
+ * ArcadeDB Gremlin implementation factory class. Utilizes a pool of ArcadeGraph to avoid creating a new instance every time.
+ *
+ * @author Luca Garulli (l.garulli@arcadedata.com)
+ */
+
+public class ArcadeGraphFactory implements Closeable {
+  private final ConcurrentLinkedQueue<PooledArcadeGraph> pooledInstances       = new ConcurrentLinkedQueue();
+  private final String                                   host;
+  private final int                                      port;
+  private final String                                   databaseName;
+  private final String                                   userName;
+  private final String                                   userPassword;
+  private       int                                      maxInstances          = 32;
+  private final AtomicInteger                            totalInstancesCreated = new AtomicInteger(0);
+
+  public class PooledArcadeGraph extends ArcadeGraph {
+    private final ArcadeGraphFactory factory;
+
+    protected PooledArcadeGraph(final ArcadeGraphFactory factory, final Configuration configuration) {
+      super(configuration);
+      this.factory = factory;
+    }
+
+    protected PooledArcadeGraph(final ArcadeGraphFactory factory, final BasicDatabase database) {
+      super(database);
+      this.factory = factory;
+    }
+
+    @Override
+    public void close() {
+      factory.release(this);
+    }
+
+    public void dispose() {
+      super.close();
+    }
+  }
+
+  /**
+   * Creates a new ArcadeGraphFactory with remote database connection. By default maximum 32 instances of ArcadeGraph
+   * can be created. You can change this configuration with the method #setMaxInstances().
+   *
+   * @param host         ArcadeDB remote server ip address or host name
+   * @param port         ArcadeDB remote server TCP/IP port
+   * @param databaseName Database name
+   * @param userName     User name
+   * @param userPassword User password
+   */
+  public ArcadeGraphFactory(final String host, final int port, final String databaseName, final String userName,
+      final String userPassword) {
+    this.host = host;
+    this.port = port;
+    this.databaseName = databaseName;
+    this.userName = userName;
+    this.userPassword = userPassword;
+  }
+
+  /**
+   * Closes the factory and dispose all the remaining ArcadeGraph instances in the pool.
+   */
+  @Override
+  public void close() {
+    while (!pooledInstances.isEmpty()) {
+      final PooledArcadeGraph instance = pooledInstances.poll();
+      if (instance != null)
+        instance.dispose();
+    }
+  }
+
+  public ArcadeGraph get() {
+    PooledArcadeGraph instance = pooledInstances.poll();
+    if (instance == null) {
+      if (totalInstancesCreated.get() >= maxInstances)
+        throw new IllegalArgumentException("Unable to create more than " + maxInstances
+            + " instances in the pool. Assure the instances were correctly released with Graph.close()");
+
+      instance = new PooledArcadeGraph(this, new RemoteDatabase(host, port, databaseName, userName, userPassword));
+      totalInstancesCreated.incrementAndGet();
+    }
+    return instance;
+  }
+
+  public void setMaxInstances(final int maxInstances) {
+    this.maxInstances = maxInstances;
+  }
+
+  public int getMaxInstances() {
+    return maxInstances;
+  }
+
+  public int getTotalInstancesCreated() {
+    return totalInstancesCreated.get();
+  }
+
+  private void release(final PooledArcadeGraph pooledArcadeGraph) {
+    pooledInstances.offer(pooledArcadeGraph);
+  }
+}
---
+++ gremlin/src/test/java/com/arcadedb/server/gremlin/RemoteGremlinFactoryIT.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2023 Arcade Data Ltd
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.arcadedb.server.gremlin;
+
+import com.arcadedb.gremlin.ArcadeGraph;
+import com.arcadedb.gremlin.ArcadeGraphFactory;
+import com.arcadedb.query.sql.executor.ResultSet;
+import com.arcadedb.remote.RemoteDatabase;
+import com.arcadedb.remote.RemoteServer;
+import com.arcadedb.server.BaseGraphServerTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.in;
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.select;
+
+public class RemoteGremlinFactoryIT extends BaseGraphServerTest {
+  private static final String DATABASE_NAME = "remote-database-factory";
+
+  @Override
+  protected boolean isCreateDatabases() {
+    return false;
+  }
+
+  @Test
+  public void okPoolRelease() {
+    try (ArcadeGraphFactory pool = new ArcadeGraphFactory("127.0.0.1", 2480, DATABASE_NAME, "root",
+        BaseGraphServerTest.DEFAULT_PASSWORD_FOR_TESTS)) {
+      for (int i = 0; i < 1_000; i++) {
+        final ArcadeGraph instance = pool.get();
+        Assertions.assertNotNull(instance);
+        instance.close();
+      }
+
+      Assertions.assertEquals(1, pool.getTotalInstancesCreated());
+    }
+  }
+
+  @Test
+  public void errorPoolRelease() {
+    try (ArcadeGraphFactory pool = new ArcadeGraphFactory("127.0.0.1", 2480, DATABASE_NAME, "root",
+        BaseGraphServerTest.DEFAULT_PASSWORD_FOR_TESTS)) {
+      for (int i = 0; i < pool.getMaxInstances(); i++) {
+        final ArcadeGraph instance = pool.get();
+        Assertions.assertNotNull(instance);
+      }
+
+      try {
+        pool.get();
+        Assertions.fail();
+      } catch (IllegalArgumentException e) {
+        // EXPECTED
+      }
+
+      Assertions.assertEquals(pool.getMaxInstances(), pool.getTotalInstancesCreated());
+    }
+  }
+
+  @BeforeEach
+  public void beginTest() {
+    super.beginTest();
+    final RemoteServer server = new RemoteServer("127.0.0.1", 2480, "root", BaseGraphServerTest.DEFAULT_PASSWORD_FOR_TESTS);
+    if (!server.exists(DATABASE_NAME))
+      server.create(DATABASE_NAME);
+  }
+
+  @AfterEach
+  public void endTest() {
+    final RemoteServer server = new RemoteServer("127.0.0.1", 2480, "root", BaseGraphServerTest.DEFAULT_PASSWORD_FOR_TESTS);
+    if (server.exists(DATABASE_NAME))
+      server.drop(DATABASE_NAME);
+    super.endTest();
+  }
+}
