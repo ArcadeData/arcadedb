09004403f8cf0b14184f3835e85804df02dff07a
lvca <lvca@users.noreply.github.com>
2023-12-26 19:23:31 -0500

    fix: issue with update of multi-page records
    This issue came out from working on issue #1391

--- engine/src/main/java/com/arcadedb/engine/LocalBucket.java
+++ engine/src/main/java/com/arcadedb/engine/LocalBucket.java
@@ -731,7 +731,7 @@ public class LocalBucket extends PaginatedComponent implements Bucket {
         } else if (lastRecordSize[0] == FIRST_CHUNK || lastRecordSize[0] == NEXT_CHUNK) {
           // CONSIDER THE CHUNK SIZE
           lastRecordSize[0] = page.readInt((int) (lastRecordPositionInPage + lastRecordSize[1]));
-          lastRecordSize[1] = INT_SERIALIZED_SIZE + LONG_SERIALIZED_SIZE;
+          lastRecordSize[1] = 1L +  INT_SERIALIZED_SIZE + LONG_SERIALIZED_SIZE;
         } else if (lastRecordSize[0] < RECORD_PLACEHOLDER_CONTENT) {
           lastRecordSize[0] *= -1L;
         }
