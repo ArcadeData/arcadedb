f7d7472df289ff20fe7256a45cbfe486a001dc7e
lvca <lvca@users.noreply.github.com>
2024-03-05 19:03:07 -0500

    fix: tentative for fixing error with using Gremlin Server
    Issue #1446

--- package/src/main/config/gremlin-server.groovy
+++ package/src/main/config/gremlin-server.groovy
@@ -29,6 +29,6 @@ globals << [hook : [
 ] as LifeCycleHook]
 
 // define the default TraversalSource to bind queries to - this one will be named "graph".
-globals << [graph : graph.traversal()]
+globals << [graph : traversal().withEmbedded(graph)]
 // define the default TraversalSource to bind queries to - this one will be named "g".
-globals << [g : graph.traversal()]
+globals << [g : traversal().withEmbedded(graph)]
