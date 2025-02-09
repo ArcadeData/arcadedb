42ef369ce28199e9a34cca3b6e4abefa0184ad43
lvca <lvca@users.noreply.github.com>
2025-02-09 00:02:33 -0500

    chore: fixed cast on inherited method

--- engine/src/main/java/com/arcadedb/graph/MutableVertex.java
+++ engine/src/main/java/com/arcadedb/graph/MutableVertex.java
@@ -91,6 +91,12 @@ public class MutableVertex extends MutableDocument implements VertexInternal {
   }
 
   @Override
+  public MutableVertex set(final Map<String, Object> properties) {
+    super.set(properties);
+    return this;
+  }
+
+  @Override
   public void setBuffer(final Binary buffer) {
     super.setBuffer(buffer);
     init();
