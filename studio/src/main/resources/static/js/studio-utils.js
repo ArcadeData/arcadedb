67f98105add5ed0f6f984be4ce5614a7121623f3
ExtReMLapin <3909752+ExtReMLapin@users.noreply.github.com>
2025-02-13 10:38:33 +0100

    fixed notify usage in JS

--- studio/src/main/resources/static/js/studio-utils.js
+++ studio/src/main/resources/static/js/studio-utils.js
@@ -47,17 +47,18 @@ function globalNotify(title, message, type) {
     {
       title: "<strong>" + title + "</strong>",
       message: message,
+    },
+    {
+      type: type,
       z_index: 100000,
       placement: {
         from: "bottom",
         align: "right",
       },
     },
-    {
-      type: type,
-    },
   );
 }
+
 
 function globalActivateTab(tab) {
   $('.nav a[href="#' + tab + '"]').tab("show");
