e2259eb993b8c986d4ede1126bb09ced0e075a37
lvca <lvca@users.noreply.github.com>
2025-02-17 02:54:08 -0500

    test: added recycle space test
    Issue #1391

--- engine/src/test/java/com/arcadedb/engine/RecordRecyclingTest.java
+++ engine/src/test/java/com/arcadedb/engine/RecordRecyclingTest.java
@@ -2,18 +2,15 @@ package com.arcadedb.engine;
 
 import com.arcadedb.database.Database;
 import com.arcadedb.database.DatabaseFactory;
+import com.arcadedb.database.RID;
 import com.arcadedb.graph.MutableVertex;
 import com.arcadedb.query.sql.executor.ResultSet;
-import com.arcadedb.utility.FileUtils;
-
+import com.arcadedb.utility.CodeUtils;
 import org.junit.jupiter.api.Test;
-
-import java.io.*;
-import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DeleteAllTest {
+public class RecordRecyclingTest {
   private final static int    TOT_RECORDS = 100_000;
   private final static String VERTEX_TYPE = "Product";
   private final static String EDGE_TYPE   = "LinkedTo";
@@ -29,12 +26,10 @@ public class DeleteAllTest {
         db.getSchema().createVertexType(VERTEX_TYPE, 1);
         db.getSchema().createEdgeType(EDGE_TYPE, 1);
 
-        for (int i = 0; i < CYCLES; i++) {
-          //System.out.println("Cycle " + i);
-//          List.of(new File(databaseFactory.getDatabasePath()).listFiles())
-//              .forEach(f -> System.out.println("- " + f.getName() + ": " + FileUtils.getSizeAsString(
-//                  f.length())));
+        RID maxVertexRID = null;
+        RID maxEdgeRID = null;
 
+        for (int i = 0; i < CYCLES; i++) {
           db.transaction(() -> {
             final MutableVertex root = db.newVertex(VERTEX_TYPE)//
                 .set("id", 0)//
@@ -49,6 +44,17 @@ public class DeleteAllTest {
             }
           });
 
+          // CHECK RECORDS HAVE BEEN RECYCLED MORE THAN 80% OF THE SPACE
+          final RID maxVertex = db.query("sql", "select from " + VERTEX_TYPE + " order by @rid desc").next().getIdentity().get();
+          if (maxVertexRID != null)
+            assertThat(maxVertex.getPosition()).isLessThan((long) (maxVertexRID.getPosition() * 1.2));
+          maxVertexRID = maxVertex;
+
+          final RID maxEdge = db.query("sql", "select from " + EDGE_TYPE + " order by @rid desc").next().getIdentity().get();
+          if (maxEdgeRID != null)
+            assertThat(maxEdge.getPosition()).isLessThan((long) (maxEdgeRID.getPosition() * 1.2));
+          maxEdgeRID = maxEdge;
+
           db.transaction(() -> {
             assertThat(db.countType(VERTEX_TYPE, true)).isEqualTo(TOT_RECORDS);
             assertThat(db.countType(EDGE_TYPE, true)).isEqualTo(TOT_RECORDS - 1);
@@ -61,7 +67,7 @@ public class DeleteAllTest {
         }
 
         final ResultSet result = db.command("sql", "check database");
-//        System.out.println(result.nextIfAvailable().toJSON());
+        // System.out.println(result.nextIfAvailable().toJSON());
       } finally {
 
         if (databaseFactory.exists())
