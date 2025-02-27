6bd113b4f994d6873da20f13b7a9a6d7ec58a3a6
Roberto Franchini <ro.franchini@gmail.com>
2025-02-27 10:24:45 +0100

    refactor PostServerCommandHandlerIT test for clarity and accuracy

--- server/src/test/java/com/arcadedb/server/http/handler/PostServerCommandHandlerIT.java
+++ server/src/test/java/com/arcadedb/server/http/handler/PostServerCommandHandlerIT.java
@@ -1,28 +1,22 @@
 package com.arcadedb.server.http.handler;
 
-import com.arcadedb.log.LogManager;
 import com.arcadedb.serializer.json.JSONObject;
 import com.arcadedb.server.BaseGraphServerTest;
 import org.junit.jupiter.api.Test;
 
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.logging.Level;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PostServerCommandHandlerIT extends BaseGraphServerTest {
 
   @Test
-  void testSetServerSettingCommnad() throws Exception {
+  void testSetDatabaseSettingCommand() throws Exception {
 
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
@@ -34,23 +28,8 @@ class PostServerCommandHandlerIT extends BaseGraphServerTest {
             "Basic " + Base64.getEncoder().encodeToString(("root:" + BaseGraphServerTest.DEFAULT_PASSWORD_FOR_TESTS).getBytes()))
         .build();
 
-
     HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
     assertThat(response.statusCode()).isEqualTo(200);
-
-    request = HttpRequest.newBuilder()
-        .uri(new URI("http://localhost:2480/api/v1/server"))
-        .GET()
-        .setHeader("Authorization",
-            "Basic " + Base64.getEncoder().encodeToString(("root:" + BaseGraphServerTest.DEFAULT_PASSWORD_FOR_TESTS).getBytes()))
-        .build();
-
-
-     response = client.send(request, BodyHandlers.ofString());
-
-    assertThat(response.statusCode()).isEqualTo(200);
-//    assertThat(JsonPa response.body()).contains("arcadedb.dateTimeFormat");
-    System.out.println("response = " + response.body());
 
   }
 }
