2832b8f1f0eefc86ab8cdeca8915dd627519801e
Roberto Franchini <ro.franchini@gmail.com>
2025-03-05 13:08:38 +0100

    #2043 add BasicHaTest and first simple resync scenario

--- network/src/main/java/com/arcadedb/remote/RemoteDatabase.java
+++ network/src/main/java/com/arcadedb/remote/RemoteDatabase.java
@@ -400,7 +400,7 @@ public class RemoteDatabase extends RemoteHttpComponent implements BasicDatabase
     checkDatabaseIsOpen();
     stats.commands.incrementAndGet();
 
-    return (ResultSet) databaseCommand("command", language, command, params, true,
+    return (ResultSet) databaseCommand("command", language, command, params, false,
         (connection, response) -> createResultSet(response));
   }
 
@@ -410,7 +410,7 @@ public class RemoteDatabase extends RemoteHttpComponent implements BasicDatabase
     stats.commands.incrementAndGet();
 
     final Map<String, Object> params = mapArgs(args);
-    return (ResultSet) databaseCommand("command", language, command, params, true,
+    return (ResultSet) databaseCommand("command", language, command, params, false,
         (connection, response) -> createResultSet(response));
   }
 
