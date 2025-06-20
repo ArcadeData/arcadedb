60e4c20b54c275cf2c34526a39f48ec4fa0d471c
Roberto Franchini <ro.franchini@gmail.com>
2025-06-20 15:29:24 +0200

    fix pre-commit

--- e2e-studio/playwright.config.ts
+++ e2e-studio/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI 
+  reporter: process.env.CI
     ? [['html'], ['junit', { outputFile: 'test-results/junit-report.xml' }]]
     : 'html',
   use: {
