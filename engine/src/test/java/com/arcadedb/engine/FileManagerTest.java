b1a8719dacceb83229ae76d76ebc5d1494eb1692
Carlos Rodrigues <carlos.rodrigues@8x8.com>
2025-07-23 11:45:42 +0100

    Added gemini suggestions

--- engine/src/test/java/com/arcadedb/engine/FileManagerTest.java
+++ engine/src/test/java/com/arcadedb/engine/FileManagerTest.java
@@ -4,6 +4,9 @@ import com.arcadedb.index.lsm.LSMTreeIndexCompacted;
 import com.arcadedb.index.lsm.LSMTreeIndexMutable;
 import com.arcadedb.index.vector.HnswVectorIndex;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,74 +22,63 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * @author carlos-rodrigues@8x8.com
  */
+@EnabledOnOs({OS.LINUX, OS.MAC})
 public class FileManagerTest {
 
+    public static final Set<String> FILE_EXT = Set.of(Dictionary.DICT_EXT,
+            LocalBucket.BUCKET_EXT, LSMTreeIndexMutable.NOTUNIQUE_INDEX_EXT, LSMTreeIndexMutable.UNIQUE_INDEX_EXT,
+            LSMTreeIndexCompacted.NOTUNIQUE_INDEX_EXT, LSMTreeIndexCompacted.UNIQUE_INDEX_EXT, HnswVectorIndex.FILE_EXT);
 
     @Test
-    void construtor_failure_noPermissionsDirectory() throws IOException {
+    void construtor_failure_noPermissionsDirectory(@TempDir Path dir) throws IOException {
         // arrange
-        Path dir = Files.createTempDirectory("noPermsDir");
+
         Set<PosixFilePermission> noPerms = EnumSet.noneOf(PosixFilePermission.class);
         Files.setPosixFilePermissions(dir, noPerms);
 
         // act and assert
         assertThrows(IllegalArgumentException.class, () -> {
-            new FileManager(dir.toFile().getAbsolutePath(), ComponentFile.MODE.READ_WRITE, Set.of(Dictionary.DICT_EXT,
-                    LocalBucket.BUCKET_EXT, LSMTreeIndexMutable.NOTUNIQUE_INDEX_EXT, LSMTreeIndexMutable.UNIQUE_INDEX_EXT,
-                    LSMTreeIndexCompacted.NOTUNIQUE_INDEX_EXT, LSMTreeIndexCompacted.UNIQUE_INDEX_EXT, HnswVectorIndex.FILE_EXT));
+            new FileManager(dir.toFile().getAbsolutePath(), ComponentFile.MODE.READ_WRITE, FILE_EXT);
         });
 
-        // cleanup
+        // reset permissions to allow cleanup
         Set<PosixFilePermission> restorePerms = PosixFilePermissions.fromString("rwx------");
         Files.setPosixFilePermissions(dir, restorePerms);
-
-        Files.deleteIfExists(dir);
     }
 
     @Test
-    void construtor_failure_parentDirectoryWithNoPermissions() throws IOException {
+    void construtor_failure_parentDirectoryWithNoPermissions(@TempDir Path dir) throws IOException {
         // arrange
-        Path dir = Files.createTempDirectory("parentNoPermsDir");
         Set<PosixFilePermission> noPerms = EnumSet.noneOf(PosixFilePermission.class);
         Files.setPosixFilePermissions(dir, noPerms);
 
         // act and assert
         assertThrows(IllegalArgumentException.class, () -> {
-            new FileManager(dir.toFile().getAbsolutePath() + "/child", ComponentFile.MODE.READ_WRITE, Set.of(Dictionary.DICT_EXT,
-                    LocalBucket.BUCKET_EXT, LSMTreeIndexMutable.NOTUNIQUE_INDEX_EXT, LSMTreeIndexMutable.UNIQUE_INDEX_EXT,
-                    LSMTreeIndexCompacted.NOTUNIQUE_INDEX_EXT, LSMTreeIndexCompacted.UNIQUE_INDEX_EXT, HnswVectorIndex.FILE_EXT));
+            new FileManager(dir.toFile().getAbsolutePath() + "/child", ComponentFile.MODE.READ_WRITE, FILE_EXT);
         });
 
         // cleanup
         Set<PosixFilePermission> restorePerms = PosixFilePermissions.fromString("rwx------");
         Files.setPosixFilePermissions(dir, restorePerms);
-
-        Files.deleteIfExists(dir);
     }
 
     @Test
-    void construtor_success_emptyDirectory() throws IOException {
+    void construtor_success_emptyDirectory(@TempDir Path dir) throws IOException {
         // arrange
-        Path dir = Files.createTempDirectory("emptyDir");
-
         // act
-        FileManager fileManager = new FileManager(dir.toFile().getAbsolutePath(), ComponentFile.MODE.READ_WRITE, Set.of(Dictionary.DICT_EXT,
-                LocalBucket.BUCKET_EXT, LSMTreeIndexMutable.NOTUNIQUE_INDEX_EXT, LSMTreeIndexMutable.UNIQUE_INDEX_EXT));
+        FileManager fileManager = new FileManager(dir.toFile().getAbsolutePath(), ComponentFile.MODE.READ_WRITE, FILE_EXT);
 
         // assert
         assertTrue(fileManager.getFiles().isEmpty());
-        // cleanup
-        Files.deleteIfExists(dir);
     }
 
     @Test
     void construtor_success_noDirectory() throws IOException {
         // arrange
-Path dir = Path.of(System.getProperty("java.io.tmpdir"),"nonExistentDir");
+        Path dir = Path.of(System.getProperty("java.io.tmpdir"), "nonExistentDir");
 
         // act
-        FileManager fileManager = new FileManager(dir.toFile().getAbsolutePath(), ComponentFile.MODE.READ_WRITE, Set.of(Dictionary.DICT_EXT,
-                LocalBucket.BUCKET_EXT, LSMTreeIndexMutable.NOTUNIQUE_INDEX_EXT, LSMTreeIndexMutable.UNIQUE_INDEX_EXT));
+        FileManager fileManager = new FileManager(dir.toFile().getAbsolutePath(), ComponentFile.MODE.READ_WRITE, FILE_EXT);
 
         // assert
         assertTrue(fileManager.getFiles().isEmpty());
