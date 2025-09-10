b549d964b1766d38b2b9354f0c5806ef76e96d0a
lvca <lvca@users.noreply.github.com>
2025-09-10 07:37:10 +0200

    fix: supported more complex math expressions in the select statement
    Fixed issue #2489
    
    (cherry picked from commit 663e8109b430037b5aec0d03428542ab38fe5fe2)

---
+++ engine/src/test/java/com/arcadedb/query/sql/function/sql/SQLMathTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2021-present Arcade Data Ltd (info@arcadedata.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: 2021-present Arcade Data Ltd (info@arcadedata.com)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.arcadedb.query.sql.function.sql;
+
+import com.arcadedb.TestHelper;
+import com.arcadedb.query.sql.executor.ResultSet;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+ * @author Luca Garulli (l.garulli@arcadedata.com)
+ */
+public class SQLMathTest {
+  @Test
+  public void testSum() throws Exception {
+    TestHelper.executeInNewDatabase("sql-math", (graph) -> {
+      try (final ResultSet rs = graph.query("sql", "select (1+1+1) as math")) {
+        assertThat(rs.next().<Integer>getProperty("math")).isEqualTo(3);
+      }
+
+      try (final ResultSet rs = graph.query("sql", "select (2+5*5) as math")) {
+        assertThat(rs.next().<Integer>getProperty("math")).isEqualTo(27);
+      }
+    });
+  }
+}
