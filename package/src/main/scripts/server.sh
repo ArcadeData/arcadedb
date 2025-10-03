d661472a56e5e00aa38095614efa25d7511cb15d
Carlos Rodrigues <carlos.rodrigues@8x8.com>
2025-10-03 18:04:03 +0100

    Cosmetic patch to avoid duplication of configurations

--- package/src/main/scripts/server.sh
+++ package/src/main/scripts/server.sh
@@ -1,29 +1,4 @@
 #!/usr/bin/env sh
-#
-# Copyright © 2021-present Arcade Data Ltd (info@arcadedata.com)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-echo ""
-echo " █████╗ ██████╗  ██████╗ █████╗ ██████╗ ███████╗██████╗ ██████╗"
-echo "██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗"
-echo "███████║██████╔╝██║     ███████║██║  ██║█████╗  ██║  ██║██████╔╝"
-echo "██╔══██║██╔══██╗██║     ██╔══██║██║  ██║██╔══╝  ██║  ██║██╔══██╗"
-echo "██║  ██║██║  ██║╚██████╗██║  ██║██████╔╝███████╗██████╔╝██████╔╝"
-echo "╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═════╝ ╚═════╝"
-echo "PLAY WITH DATA                                    arcadedb.com\n"
-
 # resolve links - $0 may be a softlink
 PRG="$0"
 
@@ -100,4 +75,4 @@ exec "$JAVA" $JAVA_OPTS \
   $ARCADEDB_JMX \
   $ARCADEDB_SETTINGS \
   -cp "$ARCADEDB_HOME/lib/*" \
-  $ARGS $* "$@" com.arcadedb.server.ArcadeDBServer
+  $ARGS "$@" com.arcadedb.server.ArcadeDBServer
