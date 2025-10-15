b4fcf828025caf47a9f4bcae6f88339b0080a54c
Roberto Franchini <ro.franchini@gmail.com>
2025-10-15 09:47:35 +0200

    fix: add missing imports for concurrency utilities in ACIDTransactionTest and RandomTestMultiThreadsTest

--- engine/src/test/java/com/arcadedb/ACIDTransactionTest.java
+++ engine/src/test/java/com/arcadedb/ACIDTransactionTest.java
@@ -46,6 +46,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
