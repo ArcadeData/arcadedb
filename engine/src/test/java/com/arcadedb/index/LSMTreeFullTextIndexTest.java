89c65d517e10410e10f28b74c9b7537f338caace
Roberto Franchini <ro.franchini@gmail.com>
2025-10-28 10:55:46 +0100

    refactor: enhance list indexing and full-text search capabilities

--- engine/src/test/java/com/arcadedb/index/LSMTreeFullTextIndexTest.java
+++ engine/src/test/java/com/arcadedb/index/LSMTreeFullTextIndexTest.java
@@ -36,67 +36,48 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LSMTreeFullTextIndexTest extends TestHelper {
   private static final int    TOT       = 10000;
-  private static final String TYPE_NAME = "V";
   private static final int    PAGE_SIZE = LSMTreeIndexAbstract.DEF_PAGE_SIZE;
+  private static final String TYPE_NAME = "Doc";
+  private static final String text      = """
+      Jay Glenn Miner (May 31, 1932 – June 20, 1994) was an American integrated circuit designer, known primarily for developing multimedia chips for the Atari 2600 and Atari 8-bit family and as the "father of the Amiga". He received a BS in EECS from UC Berkeley in 1959.[2]
+      Miner started in the electronics industry with a number of designs in the medical world, including a remote-control pacemaker.
+      He moved to Atari, Inc. in the late 1970s. One of his first successes was to combine an entire breadboard of components into a single chip, known as the TIA. The TIA was the display hardware for the Atari 2600, which would go on to sell millions. After working on the TIA he headed up the design of the follow-on chip set known as ANTIC and CTIA for which he held a patent.[3] These chips would be used for the Atari 8-bit family of home computers and the Atari 5200 video game system.
+      In the early 1980s, Jay, along with other Atari staffers, had become fed up with management and decamped. They set up another chipset project under a new company in Santa Clara, called Hi-Toro (later renamed to Amiga Corporation), where they could have creative freedom. There, they started to create a new Motorola 68000-based games console, codenamed Lorraine that could be upgraded to a computer. To raise money for the Lorraine project, Amiga Corp. designed and sold joysticks and game cartridges for popular game consoles such as the Atari 2600 and ColecoVision, as well as an odd input device called the Joyboard, essentially a joystick the player stood on. Atari continued to be interested in the team's efforts throughout this period, and funded them with $500,000 in capital in return for first use of their resulting chipset.
+      Also in the early 1980s, Jay worked on a project with Intermedics, Inc. to create their first microprocessor-based cardiac pacemaker. The microprocessor was called Lazarus and the pacemaker was eventually called Cosmos. Jay was listed co-inventor on two patents. US patent 4390022, Richard V. Calfee & Jay Miner, "Implantable device with microprocessor control", issued 1983-06-28, assigned to Intermedics, Inc. US patent 4404972, Pat L. Gordon; Richard V. Calfee & Jay Miner, "Implantable device with microprocessor control", issued 1983-06-28, assigned to Intermedics, Inc.
+      The Amiga crew, having continuing serious financial problems, had sought more monetary support from investors that entire Spring. Amiga entered into discussions with Commodore. The discussions ultimately led to Commodore wanting to purchase Amiga outright, which would (from Commodore's viewpoint) cancel any outstanding contracts - including Atari Inc.'s. So instead of Amiga delivering the chipset, Commodore delivered a check of $500,000 to Atari on Amiga's behalf, in effect returning the funds invested into Amiga for completion of the Lorraine chipset.
+      The original Amiga (1985)
+      Jay worked at Commodore-Amiga for several years, in Los Gatos, California. They made good progress at the beginning, but as Commodore management changed, they became marginalised and the original Amiga staff was fired or left out on a one-by-one basis, until the entire Los Gatos office was closed. Miner later worked as a consultant for Commodore until it went bankrupt. He was known as the 'Padre' (father) of the Amiga among Amiga users.
+      Jay always took his dog "Mitchy" (a cockapoo) with him wherever he went. While he worked at Atari, Mitchy even had her own ID-badge, and an embossing of Mitchy's paw print is visible on the inside of the Amiga 1000 top cover, alongside the signatures of the engineers who worked on it.
+      Jay endured kidney problems for most of his life, according to his wife, and relied on dialysis. His sister donated one of her own. Miner died due to complications from kidney failure at the age of 62, just two months after Commodore declared bankruptcy.
+      """;
 
   @Test
-  public void testIndexing() {
-    assertThat(database.getSchema().existsType(TYPE_NAME)).isFalse();
+  public void testFulltextIndexConsistency() {
+    database.command("sql", "CREATE DOCUMENT TYPE Doc");
+    database.command("sql", "CREATE PROPERTY Doc.text STRING");
 
-    final DocumentType type = database.getSchema().buildDocumentType().withName(TYPE_NAME).withTotalBuckets(1).create();
-    type.createProperty("text", String.class);
-    final Index typeIndex = database.getSchema()
-        .buildTypeIndex(TYPE_NAME, new String[] { "text" })
-        .withType(Schema.INDEX_TYPE.FULL_TEXT)
-        .withPageSize(PAGE_SIZE)
-        .withUnique(false)
-        .create();
-
-    assertThat(database.getSchema().existsType(TYPE_NAME)).isTrue();
-
-    final String text =
-        """
-            Jay Glenn Miner (May 31, 1932 – June 20, 1994) was an American integrated circuit designer, known primarily for developing multimedia chips for the Atari 2600 and Atari 8-bit family and as the "father of the Amiga". He received a BS in EECS from UC Berkeley in 1959.[2]
-
-            Miner started in the electronics industry with a number of designs in the medical world, including a remote-control pacemaker.
-
-            He moved to Atari, Inc. in the late 1970s. One of his first successes was to combine an entire breadboard of components into a single chip, known as the TIA. The TIA was the display hardware for the Atari 2600, which would go on to sell millions. After working on the TIA he headed up the design of the follow-on chip set known as ANTIC and CTIA for which he held a patent.[3] These chips would be used for the Atari 8-bit family of home computers and the Atari 5200 video game system.
-
-            In the early 1980s, Jay, along with other Atari staffers, had become fed up with management and decamped. They set up another chipset project under a new company in Santa Clara, called Hi-Toro (later renamed to Amiga Corporation), where they could have creative freedom. There, they started to create a new Motorola 68000-based games console, codenamed Lorraine that could be upgraded to a computer. To raise money for the Lorraine project, Amiga Corp. designed and sold joysticks and game cartridges for popular game consoles such as the Atari 2600 and ColecoVision, as well as an odd input device called the Joyboard, essentially a joystick the player stood on. Atari continued to be interested in the team's efforts throughout this period, and funded them with $500,000 in capital in return for first use of their resulting chipset.
-
-            Also in the early 1980s, Jay worked on a project with Intermedics, Inc. to create their first microprocessor-based cardiac pacemaker. The microprocessor was called Lazarus and the pacemaker was eventually called Cosmos. Jay was listed co-inventor on two patents. US patent 4390022, Richard V. Calfee & Jay Miner, "Implantable device with microprocessor control", issued 1983-06-28, assigned to Intermedics, Inc. US patent 4404972, Pat L. Gordon; Richard V. Calfee & Jay Miner, "Implantable device with microprocessor control", issued 1983-06-28, assigned to Intermedics, Inc.
-
-            The Amiga crew, having continuing serious financial problems, had sought more monetary support from investors that entire Spring. Amiga entered into discussions with Commodore. The discussions ultimately led to Commodore wanting to purchase Amiga outright, which would (from Commodore's viewpoint) cancel any outstanding contracts - including Atari Inc.'s. So instead of Amiga delivering the chipset, Commodore delivered a check of $500,000 to Atari on Amiga's behalf, in effect returning the funds invested into Amiga for completion of the Lorraine chipset.
-
-
-            The original Amiga (1985)
-            Jay worked at Commodore-Amiga for several years, in Los Gatos, California. They made good progress at the beginning, but as Commodore management changed, they became marginalised and the original Amiga staff was fired or left out on a one-by-one basis, until the entire Los Gatos office was closed. Miner later worked as a consultant for Commodore until it went bankrupt. He was known as the 'Padre' (father) of the Amiga among Amiga users.
-
-            Jay always took his dog "Mitchy" (a cockapoo) with him wherever he went. While he worked at Atari, Mitchy even had her own ID-badge, and an embossing of Mitchy's paw print is visible on the inside of the Amiga 1000 top cover, alongside the signatures of the engineers who worked on it.
-
-            Jay endured kidney problems for most of his life, according to his wife, and relied on dialysis. His sister donated one of her own. Miner died due to complications from kidney failure at the age of 62, just two months after Commodore declared bankruptcy.""";
+    // Create FULL_TEXT index with BY ITEM
+    database.command("sql", "CREATE INDEX ON Doc (text) FULL_TEXT");
 
     database.transaction(() -> {
       for (int i = 0; i < TOT; ++i) {
-        final MutableDocument v = database.newDocument(TYPE_NAME);
+        final MutableDocument v = database.newDocument("Doc");
         v.set("id", i);
         v.set("text", text);
         v.save();
       }
 
-      final List<String> keywords = ((LSMTreeFullTextIndex) ((TypeIndex) typeIndex).getIndexesOnBuckets()[0]).analyzeText(
-          ((LSMTreeFullTextIndex) ((TypeIndex) typeIndex).getIndexesOnBuckets()[0]).getAnalyzer(), new Object[] { text });
+      TypeIndex typeIndex = (TypeIndex) database.getSchema().getIndexByName("Doc[text]");
+      final List<String> keywords = ((LSMTreeFullTextIndex) typeIndex.getIndexesOnBuckets()[0]).analyzeText(
+          ((LSMTreeFullTextIndex) typeIndex.getIndexesOnBuckets()[0]).getAnalyzer(), new Object[] { text });
       assertThat(keywords.isEmpty()).isFalse();
 
       for (final String k : keywords) {
         int totalPerKeyword = 0;
 
-        for (final Index idx : ((TypeIndex) typeIndex).getIndexesOnBuckets()) {
-          if (idx instanceof TypeIndex)
-            continue;
+        for (final Index idx : typeIndex.getIndexesOnBuckets()) {
 
           final IndexCursor result = idx.get(new Object[] { k });
-
           int totalPerIndex = 0;
           while (result.hasNext()) {
             result.next();
@@ -111,18 +92,16 @@ public class LSMTreeFullTextIndexTest extends TestHelper {
       }
     });
 
-    reopenDatabase();
-
-    assertThat(database.getSchema().getIndexes()[0].getType()).isEqualTo(Schema.INDEX_TYPE.FULL_TEXT);
-
-    database.getSchema().dropIndex(typeIndex.getName());
   }
 
   @Test
   public void testIndexingComposite() {
     assertThat(database.getSchema().existsType(TYPE_NAME)).isFalse();
 
-    final DocumentType type = database.getSchema().buildDocumentType().withName(TYPE_NAME).withTotalBuckets(1).create();
+    final DocumentType type = database.getSchema().buildDocumentType()
+        .withName(TYPE_NAME)
+        .withTotalBuckets(1)
+        .create();
     type.createProperty("text", String.class);
     type.createProperty("type", String.class);
     assertThatThrownBy(() -> database.getSchema()
@@ -148,28 +127,6 @@ public class LSMTreeFullTextIndexTest extends TestHelper {
           .create();
 
       assertThat(database.getSchema().existsType("Docs")).isTrue();
-
-      final String text =
-          """
-              Jay Glenn Miner (May 31, 1932 – June 20, 1994) was an American integrated circuit designer, known primarily for developing multimedia chips for the Atari 2600 and Atari 8-bit family and as the "father of the Amiga". He received a BS in EECS from UC Berkeley in 1959.[2]
-
-              Miner started in the electronics industry with a number of designs in the medical world, including a remote-control pacemaker.
-
-              He moved to Atari, Inc. in the late 1970s. One of his first successes was to combine an entire breadboard of components into a single chip, known as the TIA. The TIA was the display hardware for the Atari 2600, which would go on to sell millions. After working on the TIA he headed up the design of the follow-on chip set known as ANTIC and CTIA for which he held a patent.[3] These chips would be used for the Atari 8-bit family of home computers and the Atari 5200 video game system.
-
-              In the early 1980s, Jay, along with other Atari staffers, had become fed up with management and decamped. They set up another chipset project under a new company in Santa Clara, called Hi-Toro (later renamed to Amiga Corporation), where they could have creative freedom. There, they started to create a new Motorola 68000-based games console, codenamed Lorraine that could be upgraded to a computer. To raise money for the Lorraine project, Amiga Corp. designed and sold joysticks and game cartridges for popular game consoles such as the Atari 2600 and ColecoVision, as well as an odd input device called the Joyboard, essentially a joystick the player stood on. Atari continued to be interested in the team's efforts throughout this period, and funded them with $500,000 in capital in return for first use of their resulting chipset.
-
-              Also in the early 1980s, Jay worked on a project with Intermedics, Inc. to create their first microprocessor-based cardiac pacemaker. The microprocessor was called Lazarus and the pacemaker was eventually called Cosmos. Jay was listed co-inventor on two patents. US patent 4390022, Richard V. Calfee & Jay Miner, "Implantable device with microprocessor control", issued 1983-06-28, assigned to Intermedics, Inc. US patent 4404972, Pat L. Gordon; Richard V. Calfee & Jay Miner, "Implantable device with microprocessor control", issued 1983-06-28, assigned to Intermedics, Inc.
-
-              The Amiga crew, having continuing serious financial problems, had sought more monetary support from investors that entire Spring. Amiga entered into discussions with Commodore. The discussions ultimately led to Commodore wanting to purchase Amiga outright, which would (from Commodore's viewpoint) cancel any outstanding contracts - including Atari Inc.'s. So instead of Amiga delivering the chipset, Commodore delivered a check of $500,000 to Atari on Amiga's behalf, in effect returning the funds invested into Amiga for completion of the Lorraine chipset.
-
-
-              The original Amiga (1985)
-              Jay worked at Commodore-Amiga for several years, in Los Gatos, California. They made good progress at the beginning, but as Commodore management changed, they became marginalised and the original Amiga staff was fired or left out on a one-by-one basis, until the entire Los Gatos office was closed. Miner later worked as a consultant for Commodore until it went bankrupt. He was known as the 'Padre' (father) of the Amiga among Amiga users.
-
-              Jay always took his dog "Mitchy" (a cockapoo) with him wherever he went. While he worked at Atari, Mitchy even had her own ID-badge, and an embossing of Mitchy's paw print is visible on the inside of the Amiga 1000 top cover, alongside the signatures of the engineers who worked on it.
-
-              Jay endured kidney problems for most of his life, according to his wife, and relied on dialysis. His sister donated one of her own. Miner died due to complications from kidney failure at the age of 62, just two months after Commodore declared bankruptcy.""";
 
       final String[] words = text.split(" ");
 
