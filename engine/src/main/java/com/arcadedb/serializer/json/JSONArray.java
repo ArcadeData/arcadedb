15b16d490a712d5cf429efe36d9ce5c25690d4c5
lvca <lvca@users.noreply.github.com>
2025-11-03 11:20:30 -0500

    chore: added check on buffer position
    There is still an issue going on, probably due to concurrent access to the same record (that is not allowed)

--- engine/src/main/java/com/arcadedb/serializer/json/JSONArray.java
+++ engine/src/main/java/com/arcadedb/serializer/json/JSONArray.java
@@ -18,8 +18,6 @@
  */
 package com.arcadedb.serializer.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
