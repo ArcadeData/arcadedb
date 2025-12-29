ad540c896d44cc4f56977e1c9c7782ffc51c0aa0
Roberto Franchini <ro.franchini@gmail.com>
2025-12-29 11:50:28 +0100

    refactor simple scenario

--- resilience/src/test/java/com/arcadedb/containers/resilience/SimpleHaScenarioIT.java
+++ resilience/src/test/java/com/arcadedb/containers/resilience/SimpleHaScenarioIT.java
@@ -24,13 +24,10 @@ public class SimpleHaScenarioIT extends ContainersTestTemplate {
   @DisplayName("Test resync after network crash with 2 sewers in HA mode")
   void twoInstancesResyncAfterNetworkCrash() throws InterruptedException, IOException {
 
-    logger.info("Creating a proxy for each arcade container");
-    final Proxy arcade1Proxy = toxiproxyClient.createProxy("arcade1Proxy", "0.0.0.0:8666", "arcade1:2424");
-    final Proxy arcade2Proxy = toxiproxyClient.createProxy("arcade2Proxy", "0.0.0.0:8667", "arcade2:2424");
 
     logger.info("Creating two arcade containers");
-    createArcadeContainer("arcade1", "{arcade2}proxy:8667", "none", "any", network);
-    createArcadeContainer("arcade2", "{arcade1}proxy:8666", "none", "any", network);
+    createArcadeContainer("arcade1", "{arcade2}arcade2:2424", "none", "any", network);
+    createArcadeContainer("arcade2", "{arcade1}arcade1:2424", "none", "any", network);
 
     logger.info("Starting the containers in sequence: arcade1 will be the leader");
     List<ServerWrapper> servers = startContainers();
@@ -50,24 +47,18 @@ public class SimpleHaScenarioIT extends ContainersTestTemplate {
 
     logger.info("Check that all the data are replicated on database 2");
     db2.assertThatUserCountIs(10);
-
-    logger.info("Disconnecting the two instances");
-    arcade1Proxy.toxics().bandwidth("CUT_CONNECTION_DOWNSTREAM", ToxicDirection.DOWNSTREAM, 0);
-    arcade1Proxy.toxics().bandwidth("CUT_CONNECTION_UPSTREAM", ToxicDirection.UPSTREAM, 0);
+    db2.assertThatPhotoCountIs(100);
 
     logger.info("Adding more data to arcade 1");
     db1.addUserAndPhotos(10, 1000);
 
     logger.info("Verifying 20 users on arcade 1");
     db1.assertThatUserCountIs(20);
+    db1.assertThatPhotoCountIs(10100);
 
-    logger.info("Verifying still only 10 users on arcade 2");
-    db2.assertThatUserCountIs(10);
     logStatus(db1, db2);
-
-    logger.info("Reconnecting instances");
-    arcade1Proxy.toxics().get("CUT_CONNECTION_DOWNSTREAM").remove();
-    arcade1Proxy.toxics().get("CUT_CONNECTION_UPSTREAM").remove();
+    logger.info("Verifying 20 users on arcade 2");
+    db2.assertThatUserCountIs(20);
 
     logger.info("Waiting for resync");
 
@@ -88,10 +79,10 @@ public class SimpleHaScenarioIT extends ContainersTestTemplate {
 
   private void logStatus(DatabaseWrapper db1, DatabaseWrapper db2) {
     logger.info("Maybe  resynced?");
-    Long users2 = db2.countUsers();
-    Long photos2 = db2.countPhotos();
     Long users1 = db1.countUsers();
     Long photos1 = db1.countPhotos();
+    Long users2 = db2.countUsers();
+    Long photos2 = db2.countPhotos();
     logger.info("Users:: {} --> {} - Photos:: {} --> {} ", users1, users2, photos1, photos2);
   }
 }
