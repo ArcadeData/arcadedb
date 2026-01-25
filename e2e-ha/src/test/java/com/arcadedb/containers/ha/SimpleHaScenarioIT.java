c82ca18fca754b0d263f2f33f072b6ab21ebcca5
Roberto Franchini <ro.franchini@gmail.com>
2026-01-25 10:53:05 +0100

    wip on tests

--- e2e-ha/src/test/java/com/arcadedb/containers/ha/SimpleHaScenarioIT.java
+++ e2e-ha/src/test/java/com/arcadedb/containers/ha/SimpleHaScenarioIT.java
@@ -3,12 +3,9 @@ package com.arcadedb.containers.ha;
 import com.arcadedb.test.support.ContainersTestTemplate;
 import com.arcadedb.test.support.DatabaseWrapper;
 import com.arcadedb.test.support.ServerWrapper;
-import eu.rekawek.toxiproxy.Proxy;
-import eu.rekawek.toxiproxy.model.ToxicDirection;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
@@ -20,9 +17,8 @@ public class SimpleHaScenarioIT extends ContainersTestTemplate {
 
   @Test
 //  @Timeout(value = 10, unit = TimeUnit.MINUTES)
-  @DisplayName("Test resync after network crash with 2 sewers in HA mode")
-  void twoInstancesResyncAfterNetworkCrash() throws InterruptedException, IOException {
-
+  @DisplayName("Test servers in HA mode")
+  void twoInstancesInLeaderReplicaConfiguration() throws InterruptedException, IOException {
     logger.info("Creating a proxy for each arcade container");
 //    final Proxy arcade1Proxy = toxiproxyClient.createProxy("arcade1Proxy", "0.0.0.0:8666", "arcade1:2424");
 //    final Proxy arcade2Proxy = toxiproxyClient.createProxy("arcade2Proxy", "0.0.0.0:8667", "arcade2:2424");
@@ -31,13 +27,25 @@ public class SimpleHaScenarioIT extends ContainersTestTemplate {
     createArcadeContainer("arcade2", "{arcade1}arcade1:2424", "none", "any", network);
 
     logger.info("Starting the containers in sequence: arcade1 will be the leader");
+//    List<ServerWrapper> servers = startContainersDeeply();
     List<ServerWrapper> servers = startContainersDeeply();
+
+    // DIAGNOSTIC: Check if containers are healthy after startup
+    logger.info("DIAGNOSTIC: Checking container health after startup");
+    Thread.sleep(5000); // Wait 5 seconds for containers to stabilize
+    diagnoseContainers();
 
     logger.info("Creating the database on the first arcade container");
     DatabaseWrapper db1 = new DatabaseWrapper(servers.getFirst(), idSupplier);
     logger.info("Creating the database on arcade server 1");
     db1.createDatabase();
     db1.createSchema();
+
+    // DIAGNOSTIC: Check if arcade2 is still running after arcade1 creates database
+    logger.info("DIAGNOSTIC: Checking container health after database creation");
+//    Thread.sleep(2000); // Wait 2 seconds
+    diagnoseContainers();
+
     DatabaseWrapper db2 = new DatabaseWrapper(servers.get(1), idSupplier);
     logger.info("Checking that the database schema is replicated");
     db1.checkSchema();
@@ -47,7 +55,17 @@ public class SimpleHaScenarioIT extends ContainersTestTemplate {
     db1.addUserAndPhotos(10, 10);
 
     logger.info("Check that all the data are replicated on database 2");
-    db2.assertThatUserCountIs(10);
+    try {
+      db2.assertThatUserCountIs(10);
+    } catch (Exception e) {
+      logger.error("DIAGNOSTIC: Exception during replication check - checking container health");
+      diagnoseContainers();
+      throw e;
+    }
+
+    // DIAGNOSTIC: Check container health after replication
+    logger.info("DIAGNOSTIC: Checking container health after initial replication");
+    diagnoseContainers();
 
     logger.info("Disconnecting the two instances");
 //    arcade1Proxy.toxics().bandwidth("CUT_CONNECTION_DOWNSTREAM", ToxicDirection.DOWNSTREAM, 0);
@@ -55,13 +73,14 @@ public class SimpleHaScenarioIT extends ContainersTestTemplate {
 
     logger.info("Adding more data to arcade 1");
     db1.addUserAndPhotos(10, 1000);
+    db2.addUserAndPhotos(10, 1000);
 
     logger.info("Verifying 20 users on arcade 1");
 //    db1.assertThatUserCountIs(20);
 
     logger.info("Verifying still only 10 users on arcade 2");
 //    db2.assertThatUserCountIs(10);
-//    logStatus(db1, db2);
+    logStatus(db1, db2);
 
     logger.info("Reconnecting instances");
 //    arcade1Proxy.toxics().get("CUT_CONNECTION_DOWNSTREAM").remove();
