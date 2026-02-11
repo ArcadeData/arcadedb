98812708234cb1df2e0cba477d3a809746093fb8
Roberto Franchini <ro.franchini@gmail.com>
2026-02-11 16:46:37 +0100

    renamed to Suite

--- engine/src/test/java/com/arcadedb/query/opencypher/tck/OpenCypherTCKSuite.java
+++ engine/src/test/java/com/arcadedb/query/opencypher/tck/OpenCypherTCKSuite.java
@@ -21,16 +21,15 @@ package com.arcadedb.query.opencypher.tck;
 import org.junit.jupiter.api.Disabled;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
-@Disabled
 @IncludeEngines("cucumber")
-@SelectClasspathResource("opencypher/tck/features")
+@SelectPackages("opencypher.tck.features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.arcadedb.query.opencypher.tck")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/tck-report.html, com.arcadedb.query.opencypher.tck.TCKReportPlugin")
 public class OpenCypherTCKSuite {
