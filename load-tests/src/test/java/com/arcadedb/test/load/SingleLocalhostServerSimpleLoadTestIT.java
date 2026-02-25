c296aa506cf78d1aee0b88017f7a36b4af9702a4
robfrank <ro.franchini@gmail.com>
2026-02-25 23:51:24 +0100

    test params increased

--- load-tests/src/test/java/com/arcadedb/test/load/SingleLocalhostServerSimpleLoadTestIT.java
+++ load-tests/src/test/java/com/arcadedb/test/load/SingleLocalhostServerSimpleLoadTestIT.java
@@ -119,15 +119,16 @@ class SingleLocalhostServerSimpleLoadTestIT {
 
     ServerWrapper server = new ServerWrapper("localhost", 2481, 50051);
     DatabaseWrapper db = new DatabaseWrapper(server, idSupplier, wordSupplier);
+
     db.createDatabase();
     db.createSchema();
 
     // Parameters for the test
     final int numOfThreads = 5; //number of threads to use to insert users and photos
-    final int numOfUsers = 200; // Each thread will create 200000 users
-    final int numOfPhotos = 5; // Each user will have 5 photos
-    final int numOfFriendship = 100; // Each thread will create 100000 friendships
-    final int numOfLike = 100; // Each thread will create 100000 likes
+    final int numOfUsers = 1000; // Each thread will create 200000 users
+    final int numOfPhotos = 10; // Each user will have 5 photos
+    final int numOfFriendship = 1000; // Each thread will create 100000 friendships
+    final int numOfLike = 1000; // Each thread will create 100000 likes
 
     int expectedUsersCount = numOfUsers * numOfThreads;
     int expectedPhotoCount = expectedUsersCount * numOfPhotos;
