32754e1b68759c615a497ca71b562d6cd6fa88d7
Roberto Franchini <ro.franchini@gmail.com>
2026-03-05 13:49:27 +0100

    fix: CONTAINSANY with method call on left-hand side (#3581) (#3582)
    (cherry picked from commit 64eb57ec399e34600198aa141294e09eedfbb15d)

--- engine/src/main/java/com/arcadedb/query/sql/parser/ContainsAllCondition.java
+++ engine/src/main/java/com/arcadedb/query/sql/parser/ContainsAllCondition.java
@@ -58,6 +58,8 @@ public class ContainsAllCondition extends BooleanExpression {
     }
     if (left instanceof Iterable<?> iterable)
       left = iterable.iterator();
+    else if (left != null && left.getClass().isArray())
+      left = MultiValue.getMultiValueIterator(left);
 
     if (left instanceof Iterator<?> leftIterator) {
       if (!(right instanceof Iterable))
