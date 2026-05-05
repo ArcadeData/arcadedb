ebb8632b636a42c7b943f5f183eec11989d64348
robfrank <ro.franchini@gmail.com>
2026-05-05 10:18:52 +0200

    fix: add support for LSM sparse vector type in BoltNetworkExecutor

--- bolt/src/main/java/com/arcadedb/bolt/BoltNetworkExecutor.java
+++ bolt/src/main/java/com/arcadedb/bolt/BoltNetworkExecutor.java
@@ -1266,6 +1266,7 @@ public class BoltNetworkExecutor extends Thread {
       case HASH -> "HASH";
       case FULL_TEXT -> "FULLTEXT";
       case LSM_VECTOR -> "VECTOR";
+      case LSM_SPARSE_VECTOR -> "SPARSE_VECTOR";
       case GEOSPATIAL -> "POINT";
     };
   }
@@ -1278,6 +1279,7 @@ public class BoltNetworkExecutor extends Thread {
       case HASH -> "hash-1.0";
       case FULL_TEXT -> "fulltext-1.0";
       case LSM_VECTOR -> "vector-2.0";
+      case LSM_SPARSE_VECTOR -> "sparse-vector-2.0";
       case GEOSPATIAL -> "point-1.0";
     };
   }
@@ -1295,6 +1297,8 @@ public class BoltNetworkExecutor extends Thread {
       return "FULLTEXT".equals(neoType);
     if (filter.startsWith("show vector index"))
       return "VECTOR".equals(neoType);
+    if (filter.startsWith("show sparse_vector index"))
+      return "SPARSE_VECTOR".equals(neoType);
     return true;
   }
 
