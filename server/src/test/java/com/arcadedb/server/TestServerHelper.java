170280c2d6798d83aaa8a8b5c4e6fd7912d1ceda
robfrank <ro.franchini@gmail.com>
2026-05-18 10:31:15 +0200

    fix: clean target/config/ in server's TestServerHelper.deleteDatabaseFolders
    RaftHTTP2ServersCreateReplicatedDatabaseIT and other Raft ITs were
    failing with HTTP 403 on basic-auth login because stale
    target/config/server-users.jsonl from a previous test run held a
    password hash created with a different SERVER_ROOT_PASSWORD.
    
    The matching cleanup was already in
    test-utils/src/main/java/com/arcadedb/test/TestServerHelper - this
    brings the server-module copy in line. The duplicate helpers will be
    consolidated in a follow-up branch.

--- server/src/test/java/com/arcadedb/server/TestServerHelper.java
+++ server/src/test/java/com/arcadedb/server/TestServerHelper.java
@@ -145,6 +145,7 @@ public abstract class TestServerHelper {
 
   public static void deleteDatabaseFolders(final int totalServers) {
     FileUtils.deleteRecursively(new File("./target/databases/"));
+    FileUtils.deleteRecursively(new File("./target/config/"));
     FileUtils.deleteRecursively(new File(GlobalConfiguration.SERVER_DATABASE_DIRECTORY.getValueAsString() + File.separator));
     for (int i = 0; i < totalServers; ++i)
       FileUtils.deleteRecursively(new File(GlobalConfiguration.SERVER_DATABASE_DIRECTORY.getValueAsString() + i + File.separator));
