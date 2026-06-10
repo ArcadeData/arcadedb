ada6f21e807e29bcad3d181da3af8b561b0a4450
robfrank <ro.franchini@gmail.com>
2026-06-10 15:57:14 +0200

    feat(observability): tag Bolt queries with protocol=bolt (#4535)

---
+++ bolt/src/test/java/com/arcadedb/bolt/BoltQueryMetricsIT.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2021-present Arcade Data Ltd (info@arcadedata.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: 2021-present Arcade Data Ltd (info@arcadedata.com)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.arcadedb.bolt;
+
+import com.arcadedb.GlobalConfiguration;
+import com.arcadedb.server.BaseGraphServerTest;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.SessionConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Verifies that queries executed over a Bolt connection are tagged with protocol="bolt"
+ * in the arcadedb.query.duration Micrometer timer.
+ */
+public class BoltQueryMetricsIT extends BaseGraphServerTest {
+
+  @Override
+  public void setTestConfiguration() {
+    super.setTestConfiguration();
+    GlobalConfiguration.SERVER_PLUGINS.setValue("Bolt:com.arcadedb.bolt.BoltProtocolPlugin");
+  }
+
+  @AfterEach
+  @Override
+  public void endTest() {
+    GlobalConfiguration.SERVER_PLUGINS.setValue("");
+    super.endTest();
+  }
+
+  private Driver getDriver() {
+    return GraphDatabase.driver(
+        "bolt://localhost:7687",
+        AuthTokens.basic("root", DEFAULT_PASSWORD_FOR_TESTS),
+        Config.builder()
+            .withoutEncryption()
+            .build()
+    );
+  }
+
+  @Test
+  void boltQueriesTaggedWithBoltProtocol() {
+    try (Driver driver = getDriver()) {
+      try (Session session = driver.session(SessionConfig.forDatabase(getDatabaseName()))) {
+        session.run("RETURN 1 AS one").consume();
+      }
+    }
+
+    final Timer timer = Metrics.globalRegistry.find("arcadedb.query.duration").tag("protocol", "bolt").timer();
+    assertThat(timer).isNotNull();
+    assertThat(timer.count()).isGreaterThanOrEqualTo(1L);
+  }
+}
