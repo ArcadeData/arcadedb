0d63cbdbac30841a3ccf434c3b8fb9ce3b4e6011
Leonardo Page <leonardo@arcadedata.com>
2026-06-13 16:46:27 +0000

    fix: Internal: validate Leonardo -> PR pipeline (additive round-trip test)
    Investigated and fixed by Leonardo for client issue https://github.com/ArcadeData/arcadedb-operations/issues/445.

---
+++ engine/src/test/java/com/arcadedb/DocumentStringRoundTripTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2021-present Arcade Data Ltd (info@arcadedata.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: 2021-present Arcade Data Ltd (info@arcadedata.com)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.arcadedb;
+
+import com.arcadedb.database.MutableDocument;
+import com.arcadedb.database.RID;
+import com.arcadedb.schema.Type;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DocumentStringRoundTripTest extends TestHelper {
+
+  @Override
+  public void beginTest() {
+    database.transaction(() -> database.getSchema().createDocumentType("Item").createProperty("name", Type.STRING));
+  }
+
+  @Test
+  void stringPropertySurvivesCloseAndReopen() {
+    final RID[] rid = new RID[1];
+
+    database.transaction(() -> {
+      final MutableDocument doc = database.newDocument("Item");
+      doc.set("name", "hello-world");
+      doc.save();
+      rid[0] = doc.getIdentity();
+    });
+
+    reopenDatabase();
+
+    database.transaction(() -> assertThat(database.lookupByRID(rid[0], true).asDocument().getString("name")).isEqualTo("hello-world"));
+  }
+}
