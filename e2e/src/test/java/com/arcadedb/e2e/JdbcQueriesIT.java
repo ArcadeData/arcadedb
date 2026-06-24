9cdd93b4372aee83a0dcfda3fa06925b154fe036
Roberto Franchini <ro.franchini@gmail.com>
2026-06-24 14:50:35 +0200

    docs: document test suites in README and align e2e test naming (#4726)
    (cherry picked from commit 40fa6cd858365c944ffa3c123d171d24a6b1a2ba)

--- e2e/src/test/java/com/arcadedb/e2e/JdbcQueriesIT.java
+++ e2e/src/test/java/com/arcadedb/e2e/JdbcQueriesIT.java
@@ -39,7 +39,7 @@ import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class JdbcQueriesTest extends ArcadeContainerTemplate {
+class JdbcQueriesIT extends ArcadeContainerTemplate {
 
   private Connection conn;
 
