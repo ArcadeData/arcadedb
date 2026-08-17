1a8c61722dd7a1cea159df3079c9bbb1c26217bb
Luca Garulli <l.garulli@arcadedata.com>
2026-08-16 21:58:49 -0400

    fix(#6267): drop the TimeUnit import the removed await left behind in SuperNodeConcurrentAppendHABenchmark
    Round 3 of the code review, its only point: the class's sole use of TimeUnit was the
    Awaitility.await(...).atMost(30, TimeUnit.SECONDS) wrapper this PR removed.

--- ha-raft/src/test/java/com/arcadedb/server/ha/raft/SuperNodeConcurrentAppendHABenchmark.java
+++ ha-raft/src/test/java/com/arcadedb/server/ha/raft/SuperNodeConcurrentAppendHABenchmark.java
@@ -34,7 +34,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 
